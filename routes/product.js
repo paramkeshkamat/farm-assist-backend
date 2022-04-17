03cc2b3d53aade16430d99a03351c324005b1436
paramkeshkamat <keshkamatparam@gmail.com>
2022-04-18 01:12:54 +0530

    third commit

--- routes/product.js
+++ routes/product.js
@@ -1,12 +1,14 @@
+/** @format */
+
 const express = require("express");
 const router = express.Router();
 
 // const auth = require("../middlewares/authMiddleware");
 // const upload = require("../middlewares/multerMiddleware");
-// const productController = require("../controllers/productController");
+const productController = require("../controllers/productController");
 
-// router.get("/", productController.getAllProducts);
-// router.get("/:id", productController.getSingleProduct);
+router.get("/", productController.getAllProducts);
+router.get("/:id", productController.getSingleProduct);
 // router.post("/", auth, productController.addProduct);
 // router.patch("/:id", auth, upload.single("productImage"), productController.updateProduct);
 // router.delete("/:id", auth, productController.deleteProduct);
